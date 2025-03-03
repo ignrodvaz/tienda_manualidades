6d2e9a62c3a177b8b034a7b2607e76ea2ff22a26
Ignacio Manuel Rodríguez Vázquez <94473780+ignrodvaz@users.noreply.github.com>
2025-03-03 19:08:22 +0100

    Update Google.php

--- app/Config/Google.php
+++ app/Config/Google.php
@@ -6,7 +6,7 @@ use CodeIgniter\Config\BaseConfig;
 
 class Google extends BaseConfig
 {
-    public $clientID = '766124376823-faak2m3edr07f3visjsugc04e1l9is4v.apps.googleusercontent.com';
-    public $clientSecret = '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';
+    public $clientID = 'ID cliente';
+    public $clientSecret = 'ID Secreto';
     public $redirectUri = 'http://localhost:8888/tienda_manualidades/public/google-callback';
 }
